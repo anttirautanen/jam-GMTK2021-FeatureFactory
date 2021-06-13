be690d029e6919f8992619b73b8c601608a818f0
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2021-06-13 18:33:13 +0300

    Remove feature related content from company page

--- Assets/Scripts/CompanyView.cs
+++ Assets/Scripts/CompanyView.cs
@@ -1,50 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class CompanyView : MonoBehaviour
 {
     public ColumnView moneyColumn;
-    public ColumnView featureColumn;
-    public Transform editTeamView;
-    public Transform hiringView;
-    private int selectedFeatureIndex = 0;
 
     private void Start()
     {
         Company.CompanyStatsUpdated += UpdateMoneyColumn;
-        Company.CompanyStatsUpdated += UpdateFeatureColumn;
         TeamView.OnTeamUpdated += UpdateMoneyColumn;
         DevTeam.TeamUpdated += UpdateMoneyColumn;
+
+        UpdateMoneyColumn();
     }
 
     private void OnDestroy()
     {
         Company.CompanyStatsUpdated -= UpdateMoneyColumn;
-        Company.CompanyStatsUpdated -= UpdateFeatureColumn;
         TeamView.OnTeamUpdated -= UpdateMoneyColumn;
         DevTeam.TeamUpdated -= UpdateMoneyColumn;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            var selectedFeature = GetSelectedFeature();
-            selectedFeature?.Release();
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SetSelectedFeature(selectedFeatureIndex + 1);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            SetSelectedFeature(selectedFeatureIndex - 1);
-        }
-
         if (Input.GetKeyDown(KeyCode.E))
         {
             Company.Instance.SetProductPrice(Company.Instance.productPrice + 10);
@@ -55,56 +32,10 @@ public class CompanyView : MonoBehaviour
             Company.Instance.SetProductPrice(Company.Instance.productPrice - 10);
         }
 
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            var selectedFeature = GetSelectedFeature();
-            if (selectedFeature != null)
-            {
-                var transformInstance = UiController.Instance.OpenView(View.EditTeam);
-                transformInstance.GetComponent<EditTeamView>().Setup(selectedFeature);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            var selectedFeature = GetSelectedFeature();
-            if (selectedFeature != null)
-            {
-                var transformInstance = UiController.Instance.OpenView(View.Hire);
-                transformInstance.GetComponentInChildren<TeamView>().Setup(GetSelectedFeature().GetTeam());
-            }
-        }
-
         if (Input.GetKeyDown(KeyCode.F))
         {
             UiController.Instance.OpenView(View.Features);
         }
-    }
-
-    private void SetSelectedFeature(int nextIndex)
-    {
-        var featuresCount = Company.Instance.GetAllFeatures().Count();
-        if (nextIndex >= featuresCount)
-        {
-            selectedFeatureIndex = 0;
-        }
-        else if (nextIndex < 0)
-        {
-            if (featuresCount == 0)
-            {
-                selectedFeatureIndex = 0;
-            }
-            else
-            {
-                selectedFeatureIndex = featuresCount - 1;
-            }
-        }
-        else
-        {
-            selectedFeatureIndex = nextIndex;
-        }
-
-        UpdateFeatureColumn();
     }
 
     private void UpdateMoneyColumn()
@@ -134,24 +65,5 @@ public class CompanyView : MonoBehaviour
             new LabelAndValueRow("Price effect", $"{Mathf.RoundToInt(Customers.Instance.GetPriceEffect() * 100)}%",
                 TextRowStyle.Secondary)
         });
-    }
-
-    private void UpdateFeatureColumn()
-    {
-        var features = Company.Instance.GetAllFeatures().ToList();
-        var featureRows = new List<FeatureRow>();
-        for (var i = 0; i < features.Count; ++i)
-        {
-            var isSelected = selectedFeatureIndex == i;
-            featureRows.Add(new FeatureRow(features[i], isSelected));
-        }
-
-        featureColumn.Set(featureRows);
-    }
-
-    private Feature GetSelectedFeature()
-    {
-        var features = Company.Instance.GetAllFeatures().ToArray();
-        return features.ElementAtOrDefault(selectedFeatureIndex);
     }
 }
