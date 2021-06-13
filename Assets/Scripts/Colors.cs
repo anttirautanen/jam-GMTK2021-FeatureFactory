d62a4ce6c63c6a8c6cb5484f5c1d01a1f12793b4
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2021-06-13 15:03:38 +0300

    Extract colors

---
+++ Assets/Scripts/Colors.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Colors
+{
+    public static readonly Color DefaultColor = new Color(0f, 0f, 0f);
+    public static readonly Color PositiveColor = new Color(0.318f, 0.502f, 0.255f);
+    public static readonly Color NegativeColor = new Color(0.698f, 0.2f, 0.2f);
+}
--- Assets/Scripts/LabelAndValueRowView.cs
+++ Assets/Scripts/LabelAndValueRowView.cs
@@ -10,9 +10,6 @@ public class LabelAndValueRowView : MonoBehaviour
     private TextRowStyle style;
     private const int DefaultFontSize = 20;
     private const int DefaultHeight = 25;
-    private static readonly Color DefaultColor = new Color(0f, 0f, 0f);
-    private static readonly Color PositiveColor = new Color(0.318f, 0.502f, 0.255f);
-    private static readonly Color NegativeColor = new Color(0.698f, 0.2f, 0.2f);
 
     public void Setup(string label, string value, TextRowStyle style)
     {
@@ -30,31 +27,31 @@ public class LabelAndValueRowView : MonoBehaviour
         {
             if (style.IsHeading)
             {
-                SetStyle(DefaultColor, 30, 50);
+                SetStyle(Colors.DefaultColor, 30, 50);
             }
             else if (style.IsPositive)
             {
-                SetStyle(PositiveColor);
+                SetStyle(Colors.PositiveColor);
             }
             else if (style.IsNegative)
             {
-                SetStyle(NegativeColor);
+                SetStyle(Colors.NegativeColor);
             }
         }
         else
         {
-            SetStyle(DefaultColor);
+            SetStyle(Colors.DefaultColor);
         }
     }
 
-    private void SetStyle(Color color, int fontSize = DefaultFontSize, int height = DefaultHeight)
+    private void SetStyle(Color colors, int fontSize = DefaultFontSize, int height = DefaultHeight)
     {
         labelText.fontSize = fontSize;
         valueText.fontSize = fontSize;
         GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
         labelText.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
         valueText.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
-        labelText.color = color;
-        valueText.color = color;
+        labelText.color = colors;
+        valueText.color = colors;
     }
 }
